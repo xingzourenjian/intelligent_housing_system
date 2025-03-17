e1fd1217a787a701d95e8c855f11226063b2bb8a
欢颜若轻歌 <wangyi31501@outlook.com>
2025-03-18 00:47:37 +0800

    云服务器的服务端增加抖音旗下的AI大模型
    原来调用的是DeepSeek的api接口，但是平均5秒左右才回消息，太慢了，影响交互体验。因此，我重新加入了抖音旗下的AI大模型，速度快了很多，平均2秒回消息。

--- 云服务器/TCP服务端+AI大模型.py
+++ 云服务器/TCP服务端+AI大模型.py
@@ -1,40 +1,32 @@
 from openai import OpenAI
-# from socket import socket, AF_INET, SOCK_STREAM
+import os
 import socket
-# AF_INET 用于网络之间的进程通信
-# SOCK_STREAM 表示用TCP协议编程
 
 """
 功能：
-    获取 DeepSeek 回复的消息
+    获取 AI大模型 回复的消息
 参数：
 返回值：
     正常：返回回复的消息 出错：False
 """
-def get_response(client, messages, model="deepseek-chat", stream=False):
+def get_response(client, messages, model, stream=False):
     try:
         response_messages = "" # 用于保存回复的消息
 
         response = client.chat.completions.create( # 获取到回复的消息
             model = model,
             messages = messages,
-            max_tokens = 1024, # 默认是4096
-            temperature = 1.3, # 通用对话
             stream = stream
         )
 
         if stream: # True：流式输出
-            # print("DeepSeek：", end="", flush=True)
             for i in response:
                 chunk_message = i.choices[0].delta.content
                 response_messages = response_messages + chunk_message # 每个字都拼接起来，保存回复的消息
-                # print(chunk_message, end="", flush=True)
-            # print() # 换行
         else:
             response_messages = response.choices[0].message.content # 保存回复的消息
-            # print(f"DeepSeek：{response.choices[0].message.content}")
 
-        messages.append({"role": "assistant", "content": response_messages}) # 添加回复的消息到历史对话记录（换成"system"无法连续对话）
+        messages.append({"role": "assistant", "content": response_messages}) # 添加回复的消息到历史对话记录
 
         return response_messages
     except Exception as e:
@@ -43,14 +35,28 @@ def get_response(client, messages, model="deepseek-chat", stream=False):
         return False
 
 
+"""
+AF_INET 用于网络之间的进程通信
+SOCK_STREAM 表示用TCP协议编程
+如果使用抖音旗下的大模型，请添加环境变量 export DOUYIN_API_KEY="8512b578-dbb0-48a1-a3df-e26cf6f35ce0"
+"""
+# DeepSeek大模型
+# api_key="sk-cc800fa4ad5e42ad89235a37786a1eb3"
+# base_url="https://api.deepseek.com"
+# model = "deepseek-chat" # 指定用哪个模型
 
-client = OpenAI(api_key="sk-cc800fa4ad5e42ad89235a37786a1eb3", base_url="https://api.deepseek.com")
-model = "deepseek-chat" # 指定用哪个模型，本次调用为V3模型。如果调用R1模型，会出错，因为它的连续对话代码不是这么写的
+# 抖音旗下大模型
+api_key=os.environ.get("DOUYIN_API_KEY") # 从环境变量中读取您的方舟API Key
+base_url="https://ark.cn-beijing.volces.com/api/v3"
+model="doubao-1.5-pro-256k-250115"
+
 messages = [
-    {"role": "system", "content": "你的名字叫大白，会根据用户提问的语言，选择回答的语言，且回答问题很简要，并且会带上颜文字来体现你的表情。"},
+    {"role": "system", "content": "你叫大白，会根据用户提问的语言，选择回答的语言，并且会带上各种表情包来体现你的面部表情。"},
     {"role": "user", "content": "你好呀！，我叫轻歌"}
+    # {"role": "assistant", "content": "我......"}
 ]
-stream = True # True：流式输出 默认是False：非流式输出
+stream = False # True：流式输出 默认是False：非流式输出
+client = OpenAI(api_key = api_key, base_url = base_url)
 
 if __name__ == '__main__':
     # 1、使用socket类创建套接字对象
@@ -73,8 +79,9 @@ if __name__ == '__main__':
     client_socket, client_address = server_socket.accept()
     print("连接客户端成功！")
 
-    # 5、使用recv()/send()方法接收/发送数据，并使用 DeepSeek回复
-    response_messages = get_response(client, messages, model, stream) #获取 DeepSeek回复
+    # 5、使用recv()/send()方法接收/发送数据
+    response_messages = get_response(client, messages, model, stream) #获取 AI大模型 的回复
+    print(response_messages)
     client_socket.send(response_messages.encode("utf-8")) # 给客户端打个招呼
 
     while True:
@@ -99,5 +106,3 @@ if __name__ == '__main__':
 
     # 6、使用close()关闭套接字
     server_socket.close()
-
-
